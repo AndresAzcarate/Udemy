bde727cf2222a6de524b54490b93f1296bee2764
Azcarate_ <asar170917@gmail.com>
2023-09-06 09:21:26 -0500

    ✨ added Variables and Data types

--- JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,26 @@
+/*
 let js = 'amazing';
 if (js === 'amazing') alert('JS is FUN!!');
-
 console.log(42 - 411 + 31);
+
+let firstName = "Andres"
+console.log(firstName)
+
+
+// Values and Variables
+let country = "Colombia"
+let continent = "America"
+let population = 51609000
+console.log( country)
+console.log( continent)
+console.log( population)
+
+// Data Types
+let isIsland = false;
+let lenguage;
+
+console.log(typeof country)
+console.log(typeof population)
+console.log(typeof isIsland)
+console.log(typeof lenguage)
+*/
