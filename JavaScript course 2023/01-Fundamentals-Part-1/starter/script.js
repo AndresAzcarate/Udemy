e14ec00b29b2fb6c6050b2dec6635b6a013842cf
Azcarate_ <asar170917@gmail.com>
2023-09-09 16:32:59 -0500

    ✨ Basic operators

--- JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
@@ -3,26 +3,44 @@ let js = 'amazing';
 if (js === 'amazing') alert('JS is FUN!!');
 console.log(42 - 411 + 31);
 
-let firstName = "Andres"
-console.log(firstName)
-*/
+let firstName = "Andres";
+console.log(firstName);
 
 // Values and Variables
-let country = "Colombia"
-let continent = "America"
-let population = 51609000
-console.log( country)
-console.log( continent)
-console.log( population)
+let country = "Colombia";
+let continent = "America";
+let population = 51609000;
+console.log( country);
+console.log( continent);
+console.log( population);
 
 // Data Types
 let isIsland = false;
-console.log(typeof country)
-console.log(typeof population)
-console.log(typeof isIsland)
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
 
 // let, const and var
 const lenguage = "Spanish";
-// lenguage = "English" // ERROR. Assignment to constant variable
-
-
+// lenguage = "English"; // ERROR. Assignment to constant variable
+*/
+// Basic operators 
+const now = 2023;
+const ageSebas = now - 1997;
+const ageDana = now - 2000;
+console.log(ageSebas, ageDana);
+console.log(ageSebas * 2, ageDana / 2, 2 ** 3);
+const firstName = "Andrés";
+const lastName = "Azcarate";
+console.log(firstName + ' ' + lastName);
+// Assingment operators
+let x = 10;
+x += 10; // x = x+10 ... 20
+x *= 10; // x = x*10 ... 200
+x -= 10; // x = x-10 ... 190
+x /= 10; // x = x/10 ... 19
+x++;     // x = x+1 ... 20
+x--;     // x = x-1 ... 19
+console.log(x);
+// Comparason operators
+console.log(ageSebas > ageDana); // <, >, <=, >=
