f82af23be2fdcb4b2585558f33ba1122b97ade7a
Azcarate_ <asar170917@gmail.com>
2023-09-10 16:25:20 -0500

    ✨Equality operators == and ===

--- JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
@@ -94,4 +94,10 @@ console.log('9' - '5');
 console.log('19' - '13' + 17);
 console.log('123' < 57);
 console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// Truthy and falsy values
+// 5 falsy values 0, '', undefined, NaN, null
 */
+// Equality operators == and ===
+// === (strict)
+// == (loose)
