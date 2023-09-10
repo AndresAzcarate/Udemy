c07a9b058c84adc82a4c587eb8d5d1917db1e25c
Azcarate_ <asar170917@gmail.com>
2023-09-10 16:05:27 -0500

    ✨Type conversion and coercion

--- JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
@@ -88,4 +88,10 @@ if(BMIMark>BMIJohn) {
   console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
 }else{
   console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
-  */
+
+// Type conversion and coercion
+console.log('9' - '5');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+*/
