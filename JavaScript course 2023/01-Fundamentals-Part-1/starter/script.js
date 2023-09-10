693d91f02949efc226523324d2c48dbbb49d0bfb
Azcarate_ <asar170917@gmail.com>
2023-09-09 19:49:23 -0500

    ✨ Challenge No1

--- JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript course 2023/01-Fundamentals-Part-1/starter/script.js
@@ -3,28 +3,28 @@ let js = 'amazing';
 if (js === 'amazing') alert('JS is FUN!!');
 console.log(42 - 411 + 31);
 
-let firstName = "Andres";
-console.log(firstName);
+// let firstName = "Andres";
+// console.log(firstName);
 
-// Values and Variables
+//  LECTURE: Values and Variables
 let country = "Colombia";
 let continent = "America";
 let population = 51609000;
-console.log( country);
-console.log( continent);
-console.log( population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// Data Types
+// LECTURE: Data Types
 let isIsland = false;
 console.log(typeof country);
 console.log(typeof population);
 console.log(typeof isIsland);
 
-// let, const and var
+// LECTURE: let, const and var
 const lenguage = "Spanish";
 // lenguage = "English"; // ERROR. Assignment to constant variable
-*/
-// Basic operators 
+
+// Basic operators
 const now = 2023;
 const ageSebas = now - 1997;
 const ageDana = now - 2000;
@@ -44,3 +44,29 @@ x--;     // x = x-1 ... 19
 console.log(x);
 // Comparason operators
 console.log(ageSebas > ageDana); // <, >, <=, >=
+
+// LECTURE: Basic operators
+console.log('Basic operators');
+console.log((population / 2) / 2);
+population++;
+console.log(population);
+const populationFinland = 6000000;
+console.log(population > populationFinland);
+console.log(33000000 > population);
+let description = "Portugal is in Europe, and its 11 millions people speak portuguese";
+console.log(description)
+
+// CHALLENGE #1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const markBMI = massMark / (heightMark ** 2);
+console.log("Mark's BMI " + markBMI);
+const johnBMI = massJohn / (heightJohn ** 2);
+console.log("John's BMI " + johnBMI);
+
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markHeigherBMI);
+*/
